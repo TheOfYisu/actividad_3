64740b6f8f56056a76bf9566f1057b0d6768e566
Jesus Daniel Garizao Mejia <jesudgm.11@outlook.com>
2024-02-01 20:39:21 -0500

    Transformando variables a AppComponent usando Pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, Pipe, PipeTransform } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { ExponentialStrengthPipe } from './prueba.pipe';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, FormsModule, ExponentialStrengthPipe],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'actividad_3';
+
+  name: string = 'Jesus';
+  lastname: string = 'Garizao';
+  mydate: Date = new Date(2003, 11, 11);
+
+  myedad: number = 25.4444444;
+
+  mymoney: string = '1000';
+
+  mytime: number = 0.1;
+
+  object: Object = {
+    name: 'Jesus',
+    lastname: 'Garizao',
+    mydate: '2003- 11- 11',
+  };
 }
